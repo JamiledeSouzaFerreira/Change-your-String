54912b6e12b1222f53d064e98b1771b6d712a6a9
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2022-02-21 20:45:21 -0300

    js básico

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function converter (){
+var btn = document.querySelector('input.btn')
+
+//event listeners
+btn.addEventListener("click", maiúsculo)
+btn.addEventListener("click", minúsculo)
+
+//functions
+function maiúsculo (){
     var txt = document.querySelector('input.txt')
-    var txt = txt.value
+    var txt = String(txt.value)
+    var res = document.querySelector('h3#res-maiúsculo')
+    res.innerHTML = txt.toUpperCase()
+}
+function minúsculo (){
+    var txt = document.querySelector('input.txt')
+    var txt = String(txt.value)
+    var res = document.querySelector('h3#res-minúsculo')
+    res.innerHTML = txt.toLowerCase()
 }
