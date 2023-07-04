b34826aaf4c128a2a230b7c64dd1eba5eb9ec143
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2023-07-04 20:09:13 -0300

    Novo JS - Fontes funcionando
    Para acrescentar novas fontes ou estilizações é necessário apenas adicionar uma nova função

--- index.js
+++ index.js
@@ -1,71 +1,23 @@
-//Fontes
-    var btn = document.querySelector('input.btn')
+//fontes
+var txtF = document.querySelector('input#txtFontes')
+var btnFontes = document.querySelector('input#btnFontes')
+btnFontes.addEventListener("click", maiúsculo)
+btnFontes.addEventListener("click", minúsculo)
+function maiúsculo (){
+    let res = document.querySelector('h3#resMaiúsculo')
+    let txt = String(txtFontes.value)
+    res.innerHTML = txt.toUpperCase()
+}
+function minúsculo (){
+    let res = document.querySelector('h3#resMinúsculo')
+    let txt = String(txtFontes.value)
+    res.innerHTML = txt.toLowerCase()
+}
+//temperatura
+var btnTemperatura = document.querySelector('imput#btnTemperatura')
+btnTemperatura.addEventListener("click", kelvin)
+btnTemperatura.addEventListener("click", faren)
+function kelvin (){
 
-    //event listeners
-    btn.addEventListener("click", maiúsculo)
-    btn.addEventListener("click", minúsculo)
-
-    //functions
-    function maiúsculo (){
-        var txt = document.querySelector('input.txt')
-        var txt = String(txt.value)
-        var res = document.querySelector('h3#res-maiúsculo')
-        res.innerHTML = txt.toUpperCase()
-    }
-    function minúsculo (){
-        var txt = document.querySelector('input.txt')
-        var txt = String(txt.value)
-        var res = document.querySelector('h3#res-minúsculo')
-        res.innerHTML = txt.toLowerCase()
-    }
-//Temperatura
-    //var celsius = Number.parseFloat(window.prompt('Escreva a temperatura em Celsius'))
-    //var kelvin = celsius + 273
-    //window.alert('A temperatura corresponde a ' + kelvin + ' graus Kelvin')
-
-    var celsius = document.querySelector('input.text')
-    var btn = document.querySelector('input#btn')
-
-
-    btn.addEventListener("click", ckelvin)
-    btn.addEventListener("click", cfarenheit)
-
-    function ckelvin (){
-        let c = Number.parseFloat(celsius.value)
-        let kelvin = c+273
-        let resposta = document.querySelector('h2.k-resposta')
-        resposta.innerHTML = ('A temperatura corresponde a '+ kelvin + ' graus Kelvin')
-    }
-
-    function cfarenheit() {
-        let c = Number.parseFloat(celsius.value)
-        let f = [9*c + (5*32)]/5
-        let resposta = document.querySelector('h2.f-resposta')
-        resposta.innerHTML = ('A temperatura corresponde a '+ f + ' graus Farenheit')
-    }
-//Moedas
-    //var real = Number.parseFloat(window.prompt('Digite o valor em real'))
-    //var conversão = real/5
-    //window.alert('O valor corresponde à ' + conversão + ' dólares' )
-    
-    function converter (){
-        var txt = document.querySelector('input#txt')
-        var txt = Number(txt.value)
-        var res = document.querySelector('p#resposta')
-        var rad = document.getElementsByName('radmoney')
-        var moeda = ''
-        //pra usarmos os radios precisaremos de uma varíavel que os represente pelo nome, e outra vazia que os represente individualmente
-        if (rad[0].checked ){
-            var dólar = txt / 5
-        res.innerHTML = (` $${dólar} doláres`)
-        } else if (rad[1].checked){
-        var libra = txt / 7
-        res.innerHTML = (`$${libra} libras`)
-        } else if(rad[2].checked){
-            var euro = txt / 5.7
-            res.innerHTML = (`$${euro} euros`)
-        } else {
-            window.alert(' Por favor selecione uma moeda')
-        }
-
-    }
+}
+//moedas
