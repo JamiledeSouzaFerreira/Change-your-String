b820697aa9be69499e5e19e9aded1cda02ad3792
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2023-07-11 14:32:04 -0300

    conversões monetárias funcionando

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function minúsculo (){
     let txt = String(txtFontes.value)
     res.innerHTML = txt.toLowerCase()
 }
-//temperatura
+//temperatura - adicionar uma mensagem pro caso de nenhum valor ter sido inserido
 var txtTemperatura = document.querySelector('input#txtTemperatura')
 var btnTemperatura = document.querySelector('input#btnTemperatura')
 btnTemperatura.addEventListener("click", kelvin)
@@ -21,14 +21,32 @@ btnTemperatura.addEventListener("click", faren)
 function kelvin (){
     let res = document.querySelector('h3#resKelvin')
     let txt = Number.parseFloat(txtTemperatura.value)
-    var kelvin = txt + 273
+    let kelvin = txt + 273
     res.innerHTML = (kelvin + ' Kelvin')
 }
 function faren(){
-    //document.body.style.backgroundColor = "red"
     let res = document.querySelector('h3#resFaren')
     let txt = Number.parseFloat(txtTemperatura.value)
-    var faren = [9*txt + (5*32)]/5
+    let faren = [9*txt + (5*32)]/5
     res.innerHTML = (faren + ' Fahrenheit')
 }
 //moedas
+var txtMoedas = document.querySelector('input#txtMoedas')
+var btnMoedas = document.querySelector('input#btnMoedas')
+btnMoedas.addEventListener("click", câmbio)
+function câmbio(){
+    let res = document.querySelector('h3#resMoeda')
+    let txt = Number.parseFloat(txtMoedas.value)
+    let rad = document.getElementsByName('radmoney')
+    let dólar = txt / 4.87
+    let libra = txt / 6.3
+    let euro = txt / 5.36
+    //pra utilizar os rádios é necessário relacioná-los pelo "name"?
+    if (rad[0].checked){
+        res.innerHTML = dólar 
+    } else if (rad[1].checked){
+        res.innerHTML = libra 
+    } else if (rad[2].checked){
+        res.innerHTML = euro
+    }
+}
